327a87a9ff55418a44f52e2a95cc2b696cf73bb0
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2024-09-16 15:20:11 -0700

    cant post :(

--- app.js
+++ app.js
@@ -1,73 +1,35 @@
+//libraries
 var express = require('express');
 var app = express();
-const PORT = 5000
+const PORT = 5000;
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 
 //public folder css and middleware
-app.use(bodyParser.json());//?
-app.use(bodyParser.urlencoded({ extended: true }));//?
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
 
 //read data from data.json
 let data = fs.readFileSync(path.join(__dirname, 'data/events.json'), 'utf8');
 let jsonData = JSON.parse(data);
 
-// app.post('/admin', (req, res) => {
-//     res.
-// })
-
 // Handle form submission
-//?????
-app.post('/submit', (req, res) => {
-    const formData = req.body;
-    
-    // Read the existing data from the JSON file
-    fs.readFile(path.join(__dirname, 'data/data.json'), 'utf8', (err, data) => {
-        if (err) {
-            // If the file doesn't exist, start with an empty array
-            if (err.code === 'ENOENT') {
-                fs.writeFile(path.join(__dirname, 'data/data.json'), 'utf8', JSON.stringify([formData], null, 2), (err) => {
-                    if (err) throw err;
-                    res.send('Form data saved.');
-                });
-            } else {
-                throw err;
-            }
-        } else {
-            // Parse the existing data and append the new data
-            const jsonData = JSON.parse(data);
-            jsonData.push(formData);
-            
-            // Write the updated data to the JSON file
-            fs.writeFile(path.join(__dirname, 'data/data.json'), 'utf8', JSON.stringify(jsonData, null, 2), (err) => {
-                if (err) throw err;
-                res.send('Form data saved.');
-            });
-        }
-    });
-});
+// app.post('/submit', (req, res) => {
+//     const { name, email, event } = req.body;
+//     const registration = `${name}|${email}|${event}\n`;
+//     fs.appendFileSync(path.join(__dirname, 'data', 'registrations.txt'), registration);
+//     res.redirect('/events');
+// });
 
-//write data to json file
-// let testEvent = {
-//     name: 'test event',attendees:25,type:"roof party"
-// }
 
-//convert object to json file
-// let newJsonData = JSON.stringify(testEvent, null, 2)
-
-//write json data to a file
-// fs.writeFileSync('data.json', newJsonData)
-
-// console.log("Data Saved Successfully")
 
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 
 //use res.render to load up an ejs view file
 
-//index page
+//events page
 app.get('/events', function(req,res){
     res.sendFile(path.join(__dirname, 'pages', 'events.ejs'));
     res.render('pages/events', {
@@ -75,13 +37,34 @@ app.get('/events', function(req,res){
     });
 });
 
-//about page
+//admin page
 app.get('/admin', function(req,res){
     res.render('pages/admin', {
         events: jsonData
     });
 });
 
+app.post('/submit', (req, res) => {
+    const { name, email, event } = req.body;
+    fs.readFile('data/registrations.json','utf8', (err, data) => {
+        if(err){console.error(err);}
+        let jsonData = JSON.stringify(JSON.parse(data));
+        jsonData.push({
+            name,
+            email,
+            event
+        });
+        fs.writeFile('data/registrations.json', JSON.stringify(jsonData) + '\n', (err) => {
+            if (err) {
+                console.error(err);
+                res.send('Error Writing to File')
+            }
+            res.redirect('/events')
+        })
+    })
+});
+
+//listening to server on port 5000
 app.listen(PORT, ()=>{
     console.log(`Server running on https://localhost:${PORT}`);
 });
